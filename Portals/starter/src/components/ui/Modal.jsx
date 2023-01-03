9ccfc9160048e13014189e20d62c958a229a9df2
MichaelGustavsson <michael.gustavsson@softtech-dev.se>
2023-01-03 10:00:08 +0100

    Updated final project

--- Portals/starter/src/components/ui/Modal.jsx
+++ Portals/starter/src/components/ui/Modal.jsx
@@ -1,23 +1,45 @@
+import ReactDOM from 'react-dom';
 import Card from './Card';
 
 import classes from './Modal.module.css';
 import OutlinedButton from './OutlinedButton';
 
+const Overlay = () => {
+  return <div className={classes.overlay} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h4>{props.title}</h4>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <OutlinedButton onClick={props.onClick}>Stäng</OutlinedButton>
+      </footer>
+    </Card>
+  );
+};
+
 const Modal = (props) => {
   return (
     <div>
-      <div className={classes.overlay} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h4>{props.title}</h4>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <OutlinedButton onClick={props.onClick}>Stäng</OutlinedButton>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Overlay />,
+        document.querySelector('#overlay-root'),
+      )}
+
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onClick={props.onClick}
+        />,
+        document.querySelector('#modal-root'),
+      )}
     </div>
   );
 };
